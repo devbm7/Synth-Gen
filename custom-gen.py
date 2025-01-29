0f9673cb5d266a03116a490199267143b4631898
Dev Makwana <devmakwana7704@gmail.com>
2025-01-29 13:17:57 +0530

    v0.1.5

--- custom-gen.py
+++ custom-gen.py
@@ -4,28 +4,44 @@ from pydantic import BaseModel
 class Country(BaseModel):
     country_name: str
     country_capital: str
-    languages: list[str]
+    country_official_languages: list[str]
 
 class CountryList(BaseModel):
     countries: list[Country]
 
+def add_two_numbers(a: int, b: int) -> int:
+    '''
+    Add two numbers
+
+    Args:
+    a: int: First number
+    b: int: Second number
+
+    Returns:
+    int: Sum of two numbers
+    '''
+    return a + b
+
 def main():
     response = chat(
         messages=[
-            # {
-            #     'role': 'user',
-            #     'content': "Generate a list of containing two countries with their capitals and languages.",
-            # },
             {
                 'role': 'user',
-                'content': "Tell me about two north american countries.",
+                'content': "Tell me about two north american countries. Respond to JSON format.",
             }
         ],
-        model='phi4:latest',
+        model='llama3.2:1b',
         format=CountryList.model_json_schema()
     )
+    print("DEBUG FULL RESPONSE: \n", response, "\n\n")
+    print("DEBUG RESPONSE MESSAGE: \n", response.message, "\n\n")
+    print("DEBUG TOKEN SPEED (tokens/s): ", f"{format((response.eval_count / response.eval_duration)*(10**9), ".5g")}", "\n\n")
     country = CountryList.model_validate_json(response.message.content)
     print(country)
+
+    print("DEBUG ADD TWO NUMBERS: ", add_two_numbers(3, 4))
+    response = chat(model="llama3.2:1b", tools=[add_two_numbers], messages=[{"role": "user", "content": "What is addition of 2 and 5?"}])
+    print(response.message)
 
 if __name__ == '__main__':
     main()
