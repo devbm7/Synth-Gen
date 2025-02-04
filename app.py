42d03390fba1c351ac396b2e485634c6df110daf
Dev Makwana <devmakwana7704@gmail.com>
2025-02-04 20:58:52 +0530

    v0.2.2

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
+import logging.config
 from ollama import chat
 import logging
+from pydantic import BaseModel
+from time import time
+
+LOG_DIR = "logs"
+DATA_DIR = "data"
+MODEL_NAME = "llama3.2:3b"
+
+logging.basicConfig(
+    filename=f"{LOG_DIR}/app.log",
+    filemode='a',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+class Data(BaseModel):
+    ID: int
+    column1: str
+
+class DataList:
+    data: list[Data]
+
+def main():
+    print("Hi")
